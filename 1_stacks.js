18851adf1d2ab03178218018826a0aefc362afb1
jake <guswns1853@gmail.com>
2020-09-21 19:06:34 +0900

    stack class 작성

--- 1_stacks.js
+++ 1_stacks.js
@@ -29,3 +29,37 @@ if (rword === word) {
 } else {
   console.log(word + " is not a palindrome.");
 }
+
+// Creates a stack
+class Stack {
+  constructor() {
+    (this.count = 0), (this.storage = {});
+  }
+
+  // Adds a value onto the end of the stack
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  // Removes and returns the value at the end of the stack
+  pop() {
+    if (this.count === 0) {
+      return undefined;
+    }
+
+    this.count--;
+    const result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  // Returns the value at the end of the stack
+  peek() {
+    return this.storage[this.count - 1];
+  }
+}
