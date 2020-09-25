2d547f9ef2892650cd2a2967030ad82bf09f2e6e
jake <guswns1853@gmail.com>
2020-09-25 22:01:18 +0900

    설명 추가

--- 3_queues.js
+++ 3_queues.js
@@ -2,6 +2,8 @@
  * Queues
  */
 
+// 배열을 통해 쉽게 구현이 가능
+// stack과 반대로 들어온 순서대로 나감 (선입선출)
 class Queue {
   constructor() {
     this.collection = [];
@@ -35,6 +37,7 @@ q.dequeue();
 console.log(q.front());
 q.print();
 
+// 우선 순위를 부여한 queue의 구현
 class PriorityQueue {
   constructor() {
     this.collection = [];
@@ -49,6 +52,7 @@ class PriorityQueue {
       let added = false;
       for (const a of this.collection) {
         if (element[1] < a[1]) {
+          // 우선순위를 확인
           this.collection.splice(this.collection.indexOf(a), 0, element);
           added = true;
           break;
@@ -61,10 +65,10 @@ class PriorityQueue {
   };
   dequeue = () => {
     const value = this.collection.shift();
-    return value[0];
+    return value[0]; // 우선순위를 제거한 값만 반환
   };
   front = () => {
-    return this.collection[0];
+    return this.collection[0]; // 이건 우선순위도 반환
   };
   size = () => {
     return this.collection.length;
