e6e19657619c868c15f32e22acd1419fb9504ae5
jake <guswns1853@gmail.com>
2020-09-30 10:22:19 +0900

    hash 함수 및 HashTable 클래스 작성

--- 5_hash_table.js
+++ 5_hash_table.js
@@ -1,3 +1,76 @@
 /**
  * Hash Table
  */
+
+const hash = (string, max) => {
+  let hash = 0;
+  for (const a of string) {
+    hash += string.charCodeAt(string.indexOf(a));
+  }
+  return hash % max;
+};
+
+class HashTable {
+  constructor() {
+    this.storage = [];
+    this.storageLimit = 14;
+  }
+
+  print = () => {
+    console.log(this.storage);
+  };
+
+  add = (key, value) => {
+    const index = hash(key, this.storageLimit);
+    if (this.storage[index] === undefined) {
+      this.storage[index] = [[key, value]];
+    } else {
+      let inserted = false;
+      for (let i = 0; i < storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          this.storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      if (inserted === false) {
+        this.storage[index].push([key, value]);
+      }
+    }
+  };
+
+  remove = (key) => {
+    const index = hash(key, this.storageLimit);
+    if (this.storage[index].length === 1 && this.storage[index][0][0] === key) {
+      delete this.storage[index];
+    } else {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          delete this.storage[index][i];
+        }
+      }
+    }
+  };
+
+  lookup = (key) => {
+    const index = hash(key, this.storageLimit);
+    if (this.storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          return this.storage[index][i][1];
+        }
+      }
+    }
+  };
+}
+
+console.log(hash("quincy", 10));
+
+let ht = new HashTable();
+ht.add("beau", "person");
+ht.add("fido", "dog");
+ht.add("rex", "dinosour");
+ht.add("tux", "penguin");
+console.log(ht.lookup("tux"));
+ht.print();
