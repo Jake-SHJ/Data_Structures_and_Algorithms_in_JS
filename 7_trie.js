81d507cf6a5c2cf361defac3655a9b77d18958ff
jake <guswns1853@gmail.com>
2020-10-06 21:18:36 +0900

    Node class 작성

---
+++ 7_trie.js
@@ -0,0 +1,16 @@
+/**
+ * Trie Data Structure
+ */
+
+class Node {
+  constructor() {
+    this.keys = new Map();
+    this.end = false;
+    this.setEnd = () => {
+      this.end = true;
+    };
+    this.isEnd = () => {
+      return this.end;
+    };
+  }
+}
