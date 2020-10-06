3511327494d250ca1a36cb6aa34a19b34c163c09
jake <guswns1853@gmail.com>
2020-10-06 21:23:12 +0900

    Trie class 작성, add 메서드 추가

--- 7_trie.js
+++ 7_trie.js
@@ -14,3 +14,21 @@ class Node {
     };
   }
 }
+
+class Trie {
+  constructor() {
+    this.root = new Node();
+  }
+
+  add = (input, node = this.root) => {
+    if (input.length == 0) {
+      node.setEnd();
+      return;
+    } else if (!node.keys.has(input[0])) {
+      node.keys.set(input[0], new Node());
+      return this.add(input.substr(1), node.keys.get(input[0]));
+    } else {
+      return this.add(input.substr(1), node.keys.get(input[0]));
+    }
+  };
+}
