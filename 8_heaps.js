18c7a1c39c41f94ee09f5638399291c632c4a69e
jake <guswns1853@gmail.com>
2020-10-08 23:52:26 +0900

    class MaxHeap 작성 및 MinHeap 누락 부분 추가

--- 8_heaps.js
+++ 8_heaps.js
@@ -52,6 +52,7 @@ class MinHeap {
       ) {
         if (this.heap[left] < this.heap[right]) {
           [this.heap[i], this.heap[left]] = [this.heap[left], this.heap[i]];
+          i = 2 * i;
         } else {
           [this.heap[i], this.heap[right]] = [this.heap[right], this.heap[i]];
           i = 2 * i + 1;
@@ -76,5 +77,73 @@ class MinHeap {
       result.push(this.remove());
     }
     return result;
+  };
+}
+
+class MaxHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  print = () => this.heap;
+
+  insert = (num) => {
+    this.heap.push(num);
+    if (this.heap.length > 2) {
+      let idx = this.heap.length - 1;
+      const floorHalfIdx = Math.floor(idx / 2);
+      while (this.heap[idx] > this.heap[floorHalfIdx]) {
+        if (idx >= 1) {
+          [this.heap[floorHalfIdx], this.heap[idx]] = [
+            this.heap[idx],
+            this.heap[floorHalfIdx],
+          ];
+          if (floorHalfIdx > 1) {
+            idx = floorHalfIdx;
+          } else {
+            break;
+          }
+        }
+      }
+    }
+  };
+
+  remove = () => {
+    let smallest = this.heap[1];
+    if (this.heap.length > 2) {
+      this.heap[1] = this.heap[this.heap.length - 1];
+      this.heap.splice(this.heap.length - 1);
+      if (this.heap.length == 3) {
+        if (this.heap[1] < this.heap[2]) {
+          [this.heap[1], this.heap[2]] = [this.heap[2], this.heap[1]];
+        }
+        return smallest;
+      }
+      let i = 1;
+      let left = 2 * i;
+      let right = 2 * i + 1;
+      while (
+        this.heap[i] <= this.heap[left] ||
+        this.heap[i] <= this.heap[right]
+      ) {
+        if (this.heap[left] > this.heap[right]) {
+          [this.heap[i], this.heap[left]] = [this.heap[left], this.heap[i]];
+          i = 2 * i;
+        } else {
+          [this.heap[i], this.heap[right]] = [this.heap[right], this.heap[i]];
+          i = 2 * i + 1;
+        }
+        left = 2 * i;
+        right = 2 * i + 1;
+        if (this.heap[left] == undefined || this.heap[right] == undefined) {
+          break;
+        }
+      }
+    } else if (this.heap.length == 2) {
+      this.heap.splice(1, 1);
+    } else {
+      return null;
+    }
+    return smallest;
   };
 }
